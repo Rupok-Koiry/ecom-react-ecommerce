a3d8c4038668141420185661dfda74da96874da2
Rupok-Koiry <koiry.rupok@gmail.com>
2024-12-13 00:00:20 +0600

    Origin Added

--- server/src/app.ts
+++ server/src/app.ts
@@ -17,10 +17,7 @@ app.use(cookieParser());
 // Middleware for enabling Cross-Origin Resource Sharing (CORS) for specified origins
 app.use(
   cors({
-    origin: [
-      'http://localhost:5173',
-      'https://ph-assignment-09-btl9.vercel.app',
-    ],
+    origin: ['http://localhost:5173', 'https://ph-assignment-09.vercel.app'],
   }),
 );
 
